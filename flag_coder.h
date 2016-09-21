f0bf8c8312f952d4bac40852472f7b018327d6b9
SilverBut <SilverBut@users.noreply.github.com>
2016-09-21 16:30:26 +0800

    Add limitations.

--- flag_coder.h
+++ flag_coder.h
@@ -4,5 +4,8 @@
 
 #define FLAG_ENCRYPT_COUNT 1000000
 
+//Key: 32bytes
+//IV:  16bytes
+//Flag:64bytes
 void FlagEncrypt(const uint8_t* key, const uint8_t* iv, const uint8_t* flag, uint8_t* encrypted);
 void FlagDecrypt(const uint8_t* key, const uint8_t* iv, const uint8_t* cipher, uint8_t* decrypted);
