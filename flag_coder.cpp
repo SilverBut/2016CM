60d2e8b270472e72ef7e0df6b07788436ae7b507
SilverBut <SilverBut@users.noreply.github.com>
2016-09-23 04:13:57 +0800

    Fixed important bug which may lead to incorrent crypt result.
    Former-commit-id: a1916dc609f3604f9c03d035bcb261ddd20a5688

--- flag_coder.cpp
+++ flag_coder.cpp
@@ -6,7 +6,7 @@ void FlagDecrypt(const uint8_t key[flag_key_len], const uint8_t iv[flag_iv_len],
 	uint8_t cipherResult[4][16];
 	uint8_t flagArray[4][16];
 	uint8_t midKey[16];
-	memcpy(flagArray, cipher, 64);
+	memcpy(cipherResult, cipher, 64);
 	crypt.PrepareKey(key, 32, &twKey);
 	for ( int loop = 0 ; loop < FLAG_ENCRYPT_COUNT ; loop++ ){
 		memcpy(midKey, iv, 16);
