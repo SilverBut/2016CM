118fb836b9f525b204d10fec5f0ef5c5bd8f3b02
SilverBut <SilverBut@users.noreply.github.com>
2016-09-22 23:43:45 +0800

    Fixed length, and make decryption need more time.
    Former-commit-id: c8465114d19064b6320b68dd2e41ca86248069e1

--- flag_coder.h
+++ flag_coder.h
@@ -3,10 +3,13 @@
 #include "common.h"
 #include "twofish.h"
 
-#define FLAG_ENCRYPT_COUNT 1000000
+#define FLAG_ENCRYPT_COUNT 10000000
 
+const static size_t flag_key_len = 32;
+const static size_t flag_iv_len  = 16;
+const static size_t flag_len = 64;
 //Key: 32bytes
 //IV:  16bytes
 //Flag:64bytes
-void FlagEncrypt(const uint8_t* key, const uint8_t* iv, const uint8_t* flag, uint8_t* encrypted);
-void FlagDecrypt(const uint8_t* key, const uint8_t* iv, const uint8_t* cipher, uint8_t* decrypted);
+void FlagEncrypt(const uint8_t key[flag_key_len], const uint8_t iv[flag_iv_len], const uint8_t flag[flag_len], uint8_t* encrypted);
+void FlagDecrypt(const uint8_t key[flag_key_len], const uint8_t iv[flag_iv_len], const uint8_t cipher[flag_len], uint8_t* decrypted);
