15dca77e24019013bcc979b031f4546affe697b1
SilverBut <SilverBut@users.noreply.github.com>
2016-09-22 17:04:51 +0800

    Add block database file and abstractor.
    Former-commit-id: 4598144689fadd2e09b8f334fb9c149fd767209d

---
+++ fetch_block_chain/abstracter.py
@@ -0,0 +1,21 @@
+import struct
+
+with open("blk00000.dat",'rb') as fd:
+    nf = open("newblocks","wb")
+    cnt=0
+    while True:
+        b = fd.read(8)
+        if b:
+            (label,size) = struct.unpack("<II", b)
+            if not label == 0xd9b4bef9:
+                print("Failed: Unexcepted token.")
+                exit(-1)
+            cnt+=1
+            print("%d at %d"%(cnt,fd.tell()))
+            nf.write(fd.read(80))
+            fd.seek(size-80,1)
+            if cnt==5000:
+                break
+        else:
+            break
+    nf.close()
